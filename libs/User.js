ee32d9772e3ca463090939810ddc8f49872cdc01
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-02-20 23:25:51 -0300

    add param validators to user methods

--- libs/User.js
+++ libs/User.js
@@ -78,6 +78,8 @@ class User {
    * @return {Promievent}
    */
   async bookWithLif(hotelAddress, unitAddress, fromDate, daysAmount, guestData, callbacks) {
+    await utils.validate.bookInfo({hotelAddress, unitAddress, fromDate, daysAmount, guestData});
+
     const fromDay = this.web3provider.utils.formatDate(fromDate);
     const cost = await this.bookings.getLifCost(unitAddress, fromDay, daysAmount);
     const enough = await this.balanceCheck(cost);
@@ -132,6 +134,7 @@ class User {
    * @return {Promievent}
    */
   async book(hotelAddress, unitAddress, fromDate, daysAmount, guestData, callbacks){
+    await utils.validate.bookInfo({hotelAddress, unitAddress, fromDate, daysAmount, guestData});
     const fromDay = this.web3provider.utils.formatDate(fromDate);
     const guestDataHex = this.web3provider.web3.utils.toHex(guestData);
 
@@ -167,6 +170,8 @@ class User {
    * @return {Boolean}
    */
   async balanceCheck(cost) {
+    await utils.validate.cost({cost});
+
     let weiCost = this.web3provider.utils.lif2LifWei(cost);
     weiCost = new this.web3provider.web3.utils.BN(weiCost);
 
